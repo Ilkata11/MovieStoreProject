70c4c11972f4d2e03ec13c6a0bed69ace004c3a7
Ilkata11 <iliyansimeonov11@hotmail.com>
2025-02-19 18:07:42 +0200

    updata

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp;
+
+internal class Program
+{
+    public delegate void MyDelegate(string msg);
+    static void Main(string[] args)
+    {
+        MyDelegate del = MethodA;
+
+        MyDelegate del2 = (string msg) => Console.WriteLine(msg);
+
+        MyDelegate del3 = new MyDelegate(MethodA);
+
+        del(msg: "HI");
+    }
+
+    static void MethodA(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
